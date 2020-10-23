be1cf22e914895ea7724aef1c3c5f255399cf145
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2020-10-23 10:03:23 -0600

    Refactor into a function.
    Make the linter happier.

--- commits_per_week.py
+++ commits_per_week.py
@@ -2,30 +2,49 @@
 """Print number of commits per week."""
 
 import os
+from typing import List
 
 from git import Repo  # type: ignore
+
+SECONDS_PER_WEEK = 60 * 60 * 24 * 7
+
+
+def commits_per_week(repo: Repo) -> List[int]:
+    """Commits per week in repo.
+
+    :param repo git.Repo: repo to report on
+    :return: list of commits, earliest commit first
+    :rtype: list[int]
+    """
+    commits = repo.iter_commits("--all")
+    commits_in_weeks = []
+
+    last_commit = next(commits)
+    week_end = last_commit.committed_date
+    week_start = week_end - SECONDS_PER_WEEK
+    commits_in_week = 1
+    for commit in commits:
+        commit_date = commit.committed_date
+        if commit_date < week_start:
+            commits_in_weeks.append(commits_in_week)
+            commits_in_week = 0
+            week_end = week_start
+            week_start = week_end - SECONDS_PER_WEEK
+        commits_in_week += 1
+    # very first commits may not make up an entire week
+    commits_in_weeks.append(commits_in_week)
+    commits_in_weeks.reverse()  # Matthew 20:16
+    return commits_in_weeks
 
 
 def main():
     """The big tent."""
-    seconds_per_week = 60 * 60 * 24 * 7
     cwd = os.getcwd()
     repo = Repo(cwd)
-    commits = repo.iter_commits("--all")
+    cpw = commits_per_week(repo)
+    for commits in cpw:
+        print(commits)
 
-    first = next(commits)
-    commits_in_week = 1
-    weeks_end = first.committed_date
-    weeks_start = weeks_end - seconds_per_week
-    for commit in commits:
-        commit_date = commit.committed_date
-        if commit_date < weeks_start:
-            print(commits_in_week)
-            commits_in_week = 0
-            weeks_end = weeks_start
-            weeks_start = weeks_end - seconds_per_week
-        commits_in_week += 1
-    print(commits_in_week)
 
 if __name__ == "__main__":
     main()
